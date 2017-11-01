7082e5d4133fde1a1738949b7ce2f2322d24ca80
Franzé Jr <franzejr@gmail.com>
2017-11-01 13:10:28 -0300

    Add rubocop

--- app/controllers/iframe_tester_controller.rb
+++ app/controllers/iframe_tester_controller.rb
@@ -1,4 +1,3 @@
 class IframeTesterController < ApplicationController
-  def index
-  end
+  def index; end
 end
--- test/controllers/iframe_tester_controller_test.rb
+++ test/controllers/iframe_tester_controller_test.rb
@@ -1,9 +1,8 @@
 require 'test_helper'
 
 class IframeTesterControllerTest < ActionDispatch::IntegrationTest
-  test "should get index" do
+  test 'should get index' do
     get iframe_tester_index_url
     assert_response :success
   end
-
 end
