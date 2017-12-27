052b827334bb39688c685bd422eec0307769611c
Franzé Jr <franzejr@gmail.com>
2017-12-27 10:53:31 -0300

    Use the public token to get offers

--- app/controllers/offers_controller.rb
+++ app/controllers/offers_controller.rb
@@ -1,5 +1,8 @@
 class OffersController < ApplicationController
   def index
-    @offers = ExchequerClient::OfferClient.new(api_key: @api_token).all
+    public_token = 'ptoken'
+    @offers = ExchequerClient::OfferClient.new(
+      api_key: @api_token,
+      public_token: public_token).all
   end
 end
